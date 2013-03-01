4d91312f66dde789ca6676416c749ddeaf34e4db
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-01 20:20:16 +0100

    Template for installing Citrix ICA Client

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install Citrix ICA Client to /Applications
+#
+# Examples
+#
+#  include citrix_ica_client
+class citrix_ica_client {
+  package { '':
+    provider => '',
+    source   => '',
+  }
+}
