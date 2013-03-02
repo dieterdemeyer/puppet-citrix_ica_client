537ca6249d4188649123ab927020aa375ac8657f
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-02 18:45:47 +0100

    Modified provider and name

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 #  include citrix_ica_client
 class citrix_ica_client {
-  package { 'Citrix ICA Client':
-    provider => 'appdmg',
+  package { 'Citrix-ICA-Client-11.2.0.dmg':
+    provider => 'pkgdmg',
     source   => 'https://yum.cegeka.be/osx_packages/Citrix-ICA-Client-11.2.0.dmg',
   }
 }
