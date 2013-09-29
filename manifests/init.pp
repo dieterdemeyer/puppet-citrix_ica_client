7c9d052aa0a9e6ca82e5552cc6c83d95fab31752
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-29 17:54:51 +0200

    Change dowload location

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,10 @@
 #
 #  include citrix_ica_client
 class citrix_ica_client {
+
   package { 'Citrix-ICA-Client-11.2.0.pkg.dmg':
     provider => 'pkgdmg',
-    source   => 'https://yum.cegeka.be/osx_packages/Citrix-ICA-Client-11.2.0.dmg',
+    source   => 'https://dl.dropboxusercontent.com/s/xidmbu8dhp5rp0s/Citrix-ICA-Client-11.2.0.dmg',
   }
+
 }
